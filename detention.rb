fa6a95814f78550b0fe2a0a602e1611e69dfab6b
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-29 17:46:45 +0000

    Done.

--- detention.rb
+++ detention.rb
@@ -1,1 +1,22 @@
 #Code your detention class here
+class Detention
+
+  attr_accessor :location, :time, :grumpy_teacher, :students
+
+  def initialize
+    @students = []
+  end
+
+  def activity
+    "collective punishment"
+  end
+
+  def add_student(name)
+    @students << name
+  end
+
+  def remove_student(name)
+    @students.delete(name)
+  end
+
+end
