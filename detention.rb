21a23a7959315896f114323b731311598187bcb4
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-29 17:36:50 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,7 +1,7 @@
 #Code your detention class here
 class Detention
 
-  attr_accessor :location, :time, :grumpy_teacher
+  attr_accessor :location, :time, :grumpy_teacher, :students
 
   def initialize
     @students = []
